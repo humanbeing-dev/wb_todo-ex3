ec4c6bcecf88492c22401158a15ff6659a240608
humanbeing-dev <maciej.sitowski@gmail.com>
2020-08-22 11:05:59 +0200

    Create API

--- todo3/settings.py
+++ todo3/settings.py
@@ -33,6 +33,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'api',
+    'rest_framework',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
